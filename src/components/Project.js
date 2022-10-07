cb987d8eff47b8fe46167e16905d256a6b714177
giankyle989 <72849075+giankyle989@users.noreply.github.com>
2022-10-07 19:47:45 +0800

    Add files via upload

--- src/components/Project.js
+++ src/components/Project.js
@@ -5,10 +5,10 @@ import WeatherApp from "../assets/WeatherApp.png";
 import Portfolio from "../assets/Portfolio.png";
 export default function Project() {
   return (
-    <div name="projects" className="w-full h-screen bg-sky-900 text-gray-300">
+    <div name="projects" className="w-full md:h-screen bg-sky-900 text-gray-300">
       {/**Container */}
       <div className="max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full">
-        <div>
+        <div className="pb-8">
           <p className="text-4xl font-bold inline border-b-4 border-[#38b6ff]">
             Projects
           </p>
