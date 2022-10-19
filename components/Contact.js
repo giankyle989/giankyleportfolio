6ebc56571f15ca23d98e8cef691365d882e75c3a
giankyle989 <72849075+giankyle989@users.noreply.github.com>
2022-10-19 21:12:11 +0800

    Add files via upload

---
+++ components/Contact.js
@@ -0,0 +1,30 @@
+import React from "react";
+
+export default function Contact() {
+  return (
+    <div
+      name="contact"
+      className="w-full h-screen bg-sky-900 text-gray-300 flex justify-center items-center p-4"
+    >
+      <form method="POST" action="https://getform.io/f/5a9f7b84-540b-45b3-9758-2f60cdac224f" className="flex flex-col max-w-[600px] w-full">
+        <div className="pb-8">
+          <p className="text-4xl font-bold inline border-b-4 border-[#38b6ff]">
+            Contact
+          </p>
+          <p className="py-4 ">
+            Submit the form below or email me! - <span className="font-bold select-all">kylelaguerta989@gmail.com</span>
+          </p>
+        </div>
+        <input className="bg-[#ccd6f6] p-2 focus:outline-none focus:border-[#38b6ff] focus:ring-4 focus:ring-[#38b6ff] text-black" type="text" placeholder="Name" name="name"></input>
+        <input
+          className="my-4 p-2 bg-[#ccd6f6] focus:outline-none focus:border-[#38b6ff] focus:ring-4 focus:ring-[#38b6ff] text-black"
+          type="text"
+          placeholder="Email"
+          name="email"
+        ></input>
+        <textarea className="bg-[#ccd6f6] p-2  focus:outline-none focus:border-[#38b6ff] focus:ring-4 focus:ring-[#38b6ff] text-black" placeholder='Message' name="message" cols='30' rows='10'></textarea>
+        <button className="text-white border-2 px-4 py-3 my-8 mx-auto hover:bg-[#38b6ff] hover:border-[#38b6ff] flex items-center" >Submit Form!</button>
+      </form>
+    </div>
+  );
+}
